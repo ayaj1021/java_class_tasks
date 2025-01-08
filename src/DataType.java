4a49cfdac24d12f1b4092be6c922cfbffac88e18
ayaj1021 <120981954+ayaj1021@users.noreply.github.com>
2025-01-08 16:19:38 +0100

    data type tasks

---
+++ src/DataType.java
@@ -0,0 +1,48 @@
+public class DataType {
+
+    public static  void main(String [] args){
+
+
+        int number = 100;
+        long numberOne = 200L;
+        float scoreAverage = 50.5f;
+        double amount = 233.00;
+        char data = 'a';
+        boolean isLoaded = true;
+        String userName = "John";
+        System.out.println("Number is " + number);
+        System.out.println("Number one value is " + numberOne);
+        System.out.println("Average score is " + scoreAverage);
+        System.out.println("The item price is " + amount);
+        System.out.println("The character is " + data);
+        System.out.println("Is gun loaded: " + isLoaded);
+        System.out.println("His name is " + userName);
+        number = 150;
+        numberOne = 2000L;
+        scoreAverage = 60.7f;
+        amount = 500.50;
+        data = 'c';
+        isLoaded = false;
+        userName = "Peter";
+        System.out.println("***********************");
+        System.out.println("New number is " + number);
+        System.out.println("New number one value is " + numberOne);
+        System.out.println("New average score is " + scoreAverage);
+        System.out.println("Updated item price is " + amount);
+        System.out.println("The new character is " + data);
+        System.out.println("Is gun loaded: " + isLoaded);
+        System.out.println("His new name is " + userName);
+
+
+
+
+
+    }
+}
+
+
+//Primitive data Types in Java
+//int, long, short, byte, bool, char, float, double
+
+//Non-Primitive data Types in Java
+//String, Array, Class, Interface, Enums, Record
